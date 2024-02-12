fd63106550122ffe009d8dc18d08b0665bd2d3ed
daniyar342 <tolomushev33@gmail.com>
2024-02-12 16:25:25 +0600

    proverka

--- company/models.py
+++ company/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# sdfjsllksjdfljsdlk
+# setlskjdflsdjf
+
+
+from django.db import models
+
+# Create your models here.
+# jdlsfjsjdfljlsfjljd
