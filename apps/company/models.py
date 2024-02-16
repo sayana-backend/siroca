0bfc8a136b828b589f79684a10f300795a98b4a3
Ebulda <izumikiotaka16@gmail.com>
2024-02-16 21:48:38 +0600

    3rd try

--- apps/company/models.py
+++ apps/company/models.py
@@ -1,23 +1,26 @@
-from django.db import models 
+from django.db import models
+
 
 class Company(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     country = models.CharField(max_length=255, verbose_name='Страна')
     manager = models.ForeignKey(
-        'user.ManagerProfile',  
+        'user.ManagerProfile',
         on_delete=models.SET_NULL,
         null=True,
         verbose_name='Менеджер компании',
         related_name='managed_companies',
         blank=True
     )
-    users = models.ManyToManyField(
+    users = models.OneToOneField(
         'user.UserProfile',
         verbose_name='Пользователи',
         related_name='companies',
+        on_delete=models.SET_NULL,
         blank=True, null=True
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
     # job_titles = models.ManyToManyField(
     #     'JobTitle',
     #     verbose_name='Должности',
@@ -33,14 +36,13 @@ class Company(models.Model):
         verbose_name = 'Компания'
         verbose_name_plural = 'Компании'
 
-    
     def get_users(self):
         return self.users.all()
 
 
 class JobTitle(models.Model):
     title = models.CharField(max_length=255, verbose_name='Должность')
-    company = models.ForeignKey(Company,  on_delete=models.CASCADE, verbose_name='Компания')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='Компания')
 
     def __str__(self):
         return self.title
