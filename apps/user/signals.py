4fc357adca982542063362204177c81c44f4413a
kasym0803 <kasymbek618618@gmail.com>
2024-02-19 13:46:30 +0300

    git add

--- apps/user/signals.py
+++ apps/user/signals.py
@@ -1,3 +1,4 @@
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,9 +11,6 @@ def create_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=UserProfile)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
 
 
 
