0bcaa7dff099739317d3fdaabeb4668724ffa92c
Ebulda <izumikiotaka16@gmail.com>
2024-03-27 17:41:35 +0600

    new server

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = ['http://localhost',
-                        'http://127.0.0.1:8000/', 'http://16.171.68.251', 'http://16.171.68.251:80']
+                        'http://127.0.0.1:8000/', 'http://13.60.17.217', 'http://13.60.17.217:80']
 
 
 
