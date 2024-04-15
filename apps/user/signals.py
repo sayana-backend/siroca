f55cbec3edf7ebc4f4f0430db88f5cf8068f901a
Mac <amantur.taalaibekov@gmail.com>
2024-04-15 11:16:48 +0600

    add delete signals

---
+++ apps/user/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from .models import CustomUser
+from django.core.exceptions import PermissionDenied
+
+
+
+
+@receiver(pre_delete, sender=CustomUser)
+def prevent_delete_first_user(sender, instance, **kwargs):
+    first_user = CustomUser.objects.first()
+    if instance.id == first_user.id:
+        raise PermissionDenied("Нельзя удалить первого пользователя!")
+
