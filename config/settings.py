f2d188fd136977e0395cb07e893fe0f8b4006f60
Ebulda <izumikiotaka16@gmail.com>
2024-04-17 16:41:25 +0600

    config ngnix

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 CSRF_TRUSTED_ORIGINS = ['http://localhost',
-                        'http://127.0.0.1:8000/', 'http://13.60.17.217', 'http://13.60.17.217:80']
+                        'http://127.0.0.1:8000/',
+                        'http://13.60.17.217',
+                        'http://13.60.17.217:80',
+                        'https://clients.siroca.com/',
+                        'http://clients.siroca.com/']
 
 
 
