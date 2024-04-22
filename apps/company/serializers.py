198780b934f51bd8785951964ad301f37449f326
Mac <amantur.taalaibekov@gmail.com>
2024-04-22 14:08:34 +0600

    change api company detail

--- apps/company/serializers.py
+++ apps/company/serializers.py
@@ -32,23 +32,12 @@ class CompanyListSerializer(serializers.ModelSerializer):
 class CompanyDetailSerializer(serializers.ModelSerializer):
     count_users = serializers.SerializerMethodField()
     users = serializers.SerializerMethodField()
-    main_manager = serializers.SlugRelatedField(slug_field='username', queryset=CustomUser.objects.all())
-    managers = serializers.SerializerMethodField()
     count_applications = serializers.SerializerMethodField()
 
     class Meta:
         model = Company
         fields = ['id', 'name', 'company_code', 'country', 'domain', 'main_manager',
                   'managers', 'count_users', 'users', 'count_applications', 'created_at', 'last_updated_at']
-
-    def get_main_manager(self, obj):
-        if obj.main_manager:
-            return f"{obj.main_manager.first_name} {obj.main_manager.surname}"
-        return None
-    
-    def get_managers(self, obj):
-        managers = obj.managers.all()
-        return [f"{manager.first_name} {manager.surname}" for manager in managers]
 
     def get_count_users(self, obj):
         return obj.get_count_users()
