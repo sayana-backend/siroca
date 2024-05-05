8532e5a1623808dc416ecbef5fdd81f61ef2ff80
kasym0803 <kasymbek618618@gmail.com>
2024-05-05 23:15:58 +0300

    add logs user id

--- apps/application/serializers.py
+++ apps/application/serializers.py
@@ -45,7 +45,7 @@ class LogsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ApplicationLogs
-        fields = ('id', 'user', 'formatted_created_at', 'field', 'initially', 'new')
+        fields = ('id', 'user', 'formatted_created_at', 'field', 'initially', 'new', 'user_id')
 
     def get_formatted_created_at(self, instance):
         return instance.created_at.strftime('%Y.%m.%d / %H:%M')
