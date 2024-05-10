9fd97a108984d6d446a7c4267b0b84df93e22e19
Ebulda <izumikiotaka16@gmail.com>
2024-05-10 17:21:43 +0600

    chcklist complited off

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -37,7 +37,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
 class UserUpdateSerializer(serializers.ModelSerializer):
     '''Update user'''
     main_company = serializers.SlugRelatedField(slug_field='name', queryset=Company.objects.all())
-    job_title = serializers.SlugRelatedField(slug_field='title', queryset=JobTitle.objects.all())
+    job_title = serializers.SlugRelatedField(slug_field='title', queryset=JobTitle.objects.all(), required=False)
 
     class Meta:
         model = CustomUser
