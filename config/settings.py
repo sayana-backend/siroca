d08bc9e8f3d46f73aa7d5b20fc72c6f34111af97
Ebulda <izumikiotaka16@gmail.com>
2024-07-15 21:05:37 +0600

    i hate it x2

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,7 @@ DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost',
-                        'http://127.0.0.1:8000/',
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/',
                         'http://13.60.163.21',
                         'http://13.60.163.21:80',
                         'https://clients.siroca.com',
